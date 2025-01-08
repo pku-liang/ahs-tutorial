257b651dadc421099ae8c122a610132e32cdbe3f
Youwei Xiao <shallwe@pku.edu.cn>
2025-01-09 00:58:23 +0800

    update README and submodules

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# ash-tutorial
+# AHS Tutorial
+
+This repository holds the code of frameworks involved in the tutorial ["AHS: An EDA toolbox for Agile Chip Front-end Design"](https://ericlyun.me/tutorial-aspdac2025/en/master/#ahs-an-eda-toolbox-for-agile-chip-front-end-design).
+
+## Installation
+
+### Docker
+
+We recommend using Docker to try out the frameworks.
+
+#### Using Built Docker Image
+
+We provide a built Docker image for the tutorial. You can pull it from Docker Hub:
+
+```bash
+docker pull ericlyun/ahs-tutorial:latest
+```
+
+#### Building Docker Image from Source
+
+You can refer to [ahs-docker](https://github.com/arch-of-shadow/ahs-docker) for instructions.
+
+### Local Installation
+
+We also provide a local installation script for the tutorial. Some prerequisites are required, please refer to [Dockerfile](https://github.com/arch-of-shadow/ahs-docker/blob/main/Dockerfile) as a reference.
+
+Then, you can run the following commands to install the frameworks:
+
+```bash
+git clone git@github.com:arch-of-shadow/ahs-tutorial.git
+cd ahs-tutorial
+git submodule update --init
+bash install.sh
+```
+
+## First Taste
+
+After installation, you can run the following commands to have the first taste of the frameworks (also validate the installation):
+
+> [!NOTE]
+> Fill this section.
--- install.sh
+++ install.sh
@@ -1,19 +1,15 @@
 #! /bin/bash
 
+echo "Downloading firtool-1.86.0..."
+wget https://github.com/llvm/circt/releases/download/firtool-1.86.0/firrtl-bin-linux-x64.tar.gz
+tar -xvf firrtl-bin-linux-x64.tar.gz
+cp -rf ./firtool-1.86.0/* $HOME/.local/
+
 echo "Building cmt2..."
 cd cmt2 && cargo build --release --all && cd ..
+
 echo "Building hestia..."
 cd hestia && cargo build --release --all && cd ..
-
-# check ksim, firtool-ksim, llc-ksim can be found 
-# Check if required tools exist in PATH
-echo "Checking ksim existence..."
-for tool in ksim firtool-ksim llc-ksim; do
-    if ! command -v $tool &> /dev/null; then
-        echo "Error: $tool not found in PATH"
-        exit 1
-    fi
-done
 
 LLVM_COMMIT="cbc378ecb87e3f31dd5aff91f2a621d500640412"
 echo "Building mlir, at llvm-project commit $LLVM_COMMIT"
@@ -32,7 +28,7 @@ cmake --install build --prefix install
 cd ..
 
 echo "Building hector..."
-cd Hector
+cd hector
 git submodule update --init --recursive
 cmake -G Ninja -DMLIR_DIR=../llvm-project/build/lib/cmake/mlir -B build
 cmake --build build
@@ -44,3 +40,34 @@ cd popa
 LLVM_DIR=../llvm-project/build/lib/cmake/llvm cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -B build
 cmake --build build
 cd ..
+
+echo "Building ksim..."
+cd ksim
+mkdir -p install
+export INSTALL_PREFIX=$PWD/install
+git submodule update --init --recursive
+cd third_party
+./setup-circt.sh
+./setup-lemon.sh
+cd ..
+mkdir -p build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_BUILD_TYPE=Release -GNinja
+ninja ksim ksim-opt
+ninja install
+echo "cp ksim->$HOME/.local/bin/ksim, firtool->$HOME/.local/bin/firtool-ksim, llc->$HOME/.local/bin/llc-ksim"
+cd ..
+cp ./install/bin/ksim $HOME/.local/bin/ksim
+cp ./install/bin/firtool $HOME/.local/bin/firtool-ksim
+cp ./install/bin/llc $HOME/.local/bin/llc-ksim
+cd ..
+
+
+# check ksim, firtool-ksim, llc-ksim can be found 
+# Check if required tools exist in PATH
+echo "Checking binary existence..."
+for tool in firtool ksim firtool-ksim llc-ksim; do
+    if ! command -v $tool &> /dev/null; then
+        echo "Error: $tool not found in PATH"
+        exit 1
+    fi
+done
