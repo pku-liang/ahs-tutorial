fec4e90ad2aa0b2cc8ee513433ea974e4796b499
Youwei Xiao <shallwe@pku.edu.cn>
2025-01-09 02:29:17 +0800

    add log in install.sh, update ksim

--- install.sh
+++ install.sh
@@ -1,69 +1,109 @@
 #! /bin/bash
 
-echo "Downloading firtool-1.86.0..."
-wget https://github.com/llvm/circt/releases/download/firtool-1.86.0/firrtl-bin-linux-x64.tar.gz
-tar -xvf firrtl-bin-linux-x64.tar.gz
-cp -rf ./firtool-1.86.0/* $HOME/.local/
+set -e
 
-echo "Building cmt2..."
-cd cmt2
-git submodule update --init --recursive
-cargo build --release --all
-cd ..
+# use install.log to record the installation process
+touch install.log
+
+echo "Downloading firtool-1.86.0..."
+if ! grep -q "firtool-1.86.0" install.log; then
+    wget https://github.com/llvm/circt/releases/download/firtool-1.86.0/firrtl-bin-linux-x64.tar.gz
+    tar -xvf firrtl-bin-linux-x64.tar.gz
+    cp -rf ./firtool-1.86.0/* $HOME/.local/
+    echo "firtool-1.86.0 downloaded" >> install.log
+else
+    echo "|-- already exists"
+fi
+
+    echo "Building cmt2..."
+if ! grep -q "cmt2" install.log; then
+    cd cmt2
+    git submodule update --init --recursive
+    cargo build --release --all
+    cd ..
+    echo "cmt2 built" >> install.log
+else
+    echo "|-- already exists"
+fi
 
 echo "Building hestia..."
-cd hestia && cargo build --release --all
-cd ..
+if ! grep -q "hestia" install.log; then
+    cd hestia && cargo build --release --all
+    cd ..
+    echo "hestia built" >> install.log
+else
+    echo "|-- already exists"
+fi
 
 LLVM_COMMIT="cbc378ecb87e3f31dd5aff91f2a621d500640412"
 echo "Building mlir, at llvm-project commit $LLVM_COMMIT"
-cd llvm-project
-git checkout $LLVM_COMMIT
-git apply -p1 ../popa/mlir_link_issue.patch
-cmake -DCMAKE_BUILD_TYPE=Release \
+if ! grep -q "llvm-project" install.log; then
+    cd llvm-project
+    git checkout $LLVM_COMMIT
+    git apply -p1 ../popa/mlir_link_issue.patch
+    cmake -DCMAKE_BUILD_TYPE=Release \
         -DLLVM_ENABLE_PROJECTS="clang;lld;mlir" \
         -DLLVM_TARGETS_TO_BUILD="Native" \
         -DLLVM_ENABLE_TERMINFO=OFF -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_LLD=OFF \
         -DLLVM_ENABLE_EH=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_BUILD_32_BITS=OFF \
         -DLLVM_ENABLE_BINDINGS=OFF \
         -G Ninja -S llvm -B build
-cmake --build build
-cmake --install build --prefix install
-cd ..
+    cmake --build build
+    cmake --install build --prefix install
+    cd ..
+    echo "llvm-project built" >> install.log
+else
+    echo "|-- already exists"
+fi
 
 echo "Building hector..."
-cd hector
-git submodule update --init --recursive
-cmake -G Ninja -DMLIR_DIR=../llvm-project/build/lib/cmake/mlir -B build
-cmake --build build
-cd ..
+if ! grep -q "hector" install.log; then
+    cd hector
+    git submodule update --init --recursive
+    cmake -G Ninja -DMLIR_DIR=../llvm-project/build/lib/cmake/mlir -B build
+    cmake --build build
+    cd ..
+    echo "hector built" >> install.log
+else
+    echo "|-- already exists"
+fi
 
 echo "Building popa..."
-export PATH=$PWD/llvm-project/install/bin:$PATH
-cd popa
-LLVM_DIR=../llvm-project/build/lib/cmake/llvm cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -B build
-cmake --build build
-cd ..
+if ! grep -q "popa" install.log; then
+    export PATH=$PWD/llvm-project/install/bin:$PATH
+    cd popa
+    LLVM_DIR=../llvm-project/build/lib/cmake/llvm cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -B build
+    cmake --build build
+    cd ..
+    echo "popa built" >> install.log
+else
+    echo "|-- already exists"
+fi
 
 echo "Building ksim..."
-cd ksim
-mkdir -p install
-export INSTALL_PREFIX=$PWD/install
-git submodule update --init --recursive
-cd third_party
-./setup-circt.sh
-./setup-lemon.sh
-cd ..
-mkdir -p build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_BUILD_TYPE=Release -GNinja
-ninja ksim ksim-opt
-ninja install
-echo "cp ksim->$HOME/.local/bin/ksim, firtool->$HOME/.local/bin/firtool-ksim, llc->$HOME/.local/bin/llc-ksim"
-cd ..
-cp ./install/bin/ksim $HOME/.local/bin/ksim
-cp ./install/bin/firtool $HOME/.local/bin/firtool-ksim
-cp ./install/bin/llc $HOME/.local/bin/llc-ksim
-cd ..
+if ! grep -q "ksim" install.log; then
+    cd ksim
+    mkdir -p install
+    export INSTALL_PREFIX=$PWD/install
+    git submodule update --init --recursive
+    cd third_party
+    ./setup-circt.sh
+    ./setup-lemon.sh
+    cd ..
+    mkdir -p build && cd build
+    cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_BUILD_TYPE=Release -GNinja
+    ninja ksim ksim-opt
+    ninja install
+    echo "cp ksim->$HOME/.local/bin/ksim, firtool->$HOME/.local/bin/firtool-ksim, llc->$HOME/.local/bin/llc-ksim"
+    cd ..
+    cp ./install/bin/ksim $HOME/.local/bin/ksim
+    cp ./install/bin/firtool $HOME/.local/bin/firtool-ksim
+    cp ./install/bin/llc $HOME/.local/bin/llc-ksim
+    cd ..
+    echo "ksim built" >> install.log
+else
+    echo "|-- already exists"
+fi
 
 
 # check ksim, firtool-ksim, llc-ksim can be found 
@@ -75,3 +115,5 @@ for tool in firtool ksim firtool-ksim llc-ksim; do
         exit 1
     fi
 done
+
+echo "Installation completed" >> install.log
